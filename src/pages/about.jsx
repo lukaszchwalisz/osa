d89dbf8f0859a8bf48c7dcd664300d13dd9c2798
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-03-19 23:29:01 +0100

    add nav sites & divider

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,9 @@
 const About = () => {
   return (
     <>
-    <h1 className="text-cyan-500 text-center font-bold bg-yellow-200">About</h1>
+    <hr className="my-5 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50"/>
+    <h1 className="text-slate-500 text-center font-bold">About</h1>
+    <hr className="my-5 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50"/>
      </>
      );
   }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,9 @@
 const Home = () => {
   return (
     <>
-    <h1 className="text-cyan-500 text-center font-bold bg-yellow-300">Home</h1>
+   <hr className="my-5 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50"/>
+    <h1 className="text-slate-500 text-center font-bold">Home</h1>
+    <hr className="my-5 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50"/>
      </>
      );
 }
