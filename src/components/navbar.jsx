51f94f8f830815cec2ef90418174e49061685f37
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-03-20 17:48:29 +0100

    home icon & stuff

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,9 @@ export function Navbar() {
             <ul  className="flex flex-row justify-end">
                 <span className="flex flex-center flex-1"><li><Link to="/osa/"><img className="w-64" src="https://raw.githubusercontent.com/lukaszchwalisz/osa/main/src/assets/images/Web_logo_fit2.png" alt="logo OSA"></img></Link></li></span>
                 <li className="text-slate-500 font-bold font-mono space-x-2 p-4">
-                <Link to="/osa/">Home</Link>
+                <Link to="/osa/"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg></Link>
                 </li>
                 <li className="text-slate-400 font-bold font-mono hover:text-slate-500 space-x-2 p-4">
                 <Link to="/about">Stowarzyszenie</Link>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,12 @@
 const Home = () => {
   return (
     <>
-   
-    <h1 className="text-slate-500 text-center font-bold">STRONA W PRZYGOTOWANIU !</h1>
-    <h1 className="text-slate-400 text-center">Prosimy o cierpliwość.</h1>
-    
-     </>
-     );
+      <div className="flex flex-col h-screen">
+          <div className="m-auto"><h1 className="text-slate-500 text-center font-bold text-4xl">STRONA W PRZYGOTOWANIU ! </h1><div className="m-auto"><h1 className="text-slate-400 text-center text-4xl">Prosimy o cierpliwość.</h1></div></div>
+          
+      </div>
+    </>   
+  );
 }
 
 export default Home;
