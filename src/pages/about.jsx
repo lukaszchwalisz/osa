5c4ec1d604d94d692c5802ae694c8d3abf527f89
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-03-20 20:10:09 +0100

    add content to about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,156 @@
+/* eslint-disable react/no-unescaped-entities */
 
 
 const About = () => {
   return (
     <>
-    <hr className="my-5 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50"/>
-    <h1 className="text-slate-500 text-center font-bold">About</h1>
-    <hr className="my-5 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50"/>
+
+   <article className="flex flex-row">
+   <div className="basis-1/4"></div>
+   <div className="basis-1/2">
+      <h1 className="text-purple-700 font-bold">Trochę historii</h1>
+      <p className="text-slate-500 my-4">W Polsce stowarzyszenia antymobbingowe zaczęły powstawać od roku 2001. 
+        Jako pierwsze powstało Ogólnopolskie Stowarzyszenie Antymobbingowe OSA z siedzibą w Gdańsku. 
+        Pomysł utworzenia stowarzyszenia zrodził się w dniu 17.09.2001r. na Konferencji zorganizowanej 
+        przez Korporację Organizacji Samopomocowych KOS. Grupa inicjatywna zrealizowała ten pomysł 17 października
+        2001 r. organizując zebranie założycielskie. W dniu 19 października 2001r. zgłoszono rejestrację w 
+        Krajowym Rejestrze Sądowym. W trakcie rejestracji w dniu 19.01.2002r. nastąpiła zmiana składu Komitetu 
+        Założycielskiego - Pani Barbara Grabowska pismami z dnia 13.01.2002r. i 16.01.2002r. wycofała się z 
+        komitetu założycielskiego rezygnując z członkostwa i miana założyciela Stowarzyszenia. 
+        Stowarzyszenie zostało zarejestrowane w KRS w dniu 11 lutego 2002r. pod pozycją 0000088951. 
+        Następnie kolejno powstawały Stowarzyszenie Antymobbingowe pod patronatem Barbary Grabowskiej 
+        z siedzibą w Gdańsku, Krajowe Stowarzyszenie Antymobbingowe z siedzibą we Wrocławiu, 
+        Stowarzyszenie Centrum Antymobbingowe z siedzibą w Warszawie.
+      </p>
+    </div>
+    </article>
+
+    <article className="flex flex-row">
+    <div className="basis-1/4"></div>
+    <div className="basis-1/2">
+    <h1 className="text-purple-700 font-bold">Ogólnopolskie Stowarzyszenie Antymobbingowe "OSA"</h1>
+      <p className="text-slate-500 my-4">
+        Ogólnopolskie Stowarzyszenie Antymobbingowe "OSA" powstało w odpowiedzi na zapotrzebowanie społeczne w zakresie pomocy prawnej i
+        psychologicznej osób pokrzywdzonych w miejscu pracy. Pomaga poszkodowanym odnaleźć się w takich trudnych sytuacjach i znaleźć rozwiązanie.
+        Jest organizacją o statusie pożytku publicznego. Zakres przestrzenny działania obejmuje teren całej Polski. Swoją siedzibę ma w Gdańsku
+        przy ul. Warzywniczej 10/C/7. Skupia w swoich szeregach członków i wolontariuszy, z których pierwsza część osobiście doświadczyła działań
+        mobbingowych lub zetknęła się z nimi w swoim otoczeniu, a druga część to osoby świadome zagrożeń, jakie niesie ze sobą mobbing i popierające
+    </p>
+    </div>
+    </article>
+
+    <article className="flex flex-row">
+    <div className="basis-1/4"></div>
+    <div className="basis-1/2">
+    <p className="text-slate-500 my-1">
+      Misją Stowarzyszenia jest niesienie pomocy osobom poszkodowanym na skutek mobbingu oraz kształtowanie środowiska pracy wolnego od wszelkiej agresji,
+       przemocy psychicznej i fizycznej. W oparciu o nasze doświadczenie pragniemy przyczyniać się do rozwoju kapitału ludzkiego sprzyjającemu zanikowi zjawiska
+        mobbingu w miejscu pracy. Strategią Stowarzyszenia jest stworzenie sieci pomocowej na terenie całego kraju poprzez tzw. Punkty Konsultacyjne Zarządu OSA,
+         prowadzące działalność na swoim terenie. Punkty Konsultacyjne Zarządu OSA ściśle współpracują z Zarządem - są jego bezpośrednim poszerzeniem i reprezentacją
+          na danym terenie. Punkt prowadzony jest przez członka Stowarzyszenia, który spełnia warunki wstąpienia do Stowarzyszenia i chce podjąć się takiej funkcji.
+           Zarząd Stowarzyszenia w razie potrzeby koordynuje działanie punktów oraz monitoruje i kontroluje całość działalności. Utrzymuje też kontakt z osobami pokrzywdzonymi
+            z terenu całej Polski w formie korespondencji internetowej, pocztowej i telefonicznej.</p>
+    </div>
+    </article>
+
+    <article className="flex flex-row">
+    <div className="basis-1/4"></div>
+    <div className="basis-1/2">
+    <p className="text-slate-500 my-1">
+    Stowarzyszenie opiera swoją działalność na nieodpłatnym świadczeniu pomocy przez członków stowarzyszenia i osoby współpracujące oraz na pracy wolontariuszy. 
+    Na swoją działalność pozyskuje środki finansowe ze składek członkowskich, darowizn, sponsoringu oraz dotacji z instytucji samorządowych, państwowych skierowanych
+     na konkretne programy pomocowe. Aspekt ten jest istotnym czynnikiem ograniczającym działalność stowarzyszenia z uwagi na niewielkie możliwości ich pozyskiwania.
+      Stowarzyszenie od początku swojego istnienia prowadzi nieodpłatną pomoc psychologiczno-prawną w formie informacji, edukacji, poradnictwa. Organizuje i prowadzi
+       samopomocowe grupy wsparcia dla osób pokrzywdzonych przemocą w pracy. W programach grup znajdują się elementy prawne mobbingu. Prowadzi badania grup pracowniczych
+        pod kątem występowania zjawiska mobbingu. Pomaga wielu osobom z całego kraju w pisaniu przez nich prac licencjackich, magisterskich, podyplomowych i doktoranckich
+         na temat mobbingu, udostępniając im materiał dotyczący rozwiązań poszczególnych przypadków. Prowadzi szkolenia, doradztwo dla pracodawców w zakresie przeciwdziałania
+          mobbingowi.</p>
+      </div>
+    </article>
+
+    <article className="flex flex-row">
+    <div className="basis-1/4"></div>
+    <div className="basis-1/2">
+    <p className="text-slate-500 my-1">
+    Świadomi niedoskonałości sposobów realizacji podstawowych regulacji prawnych, aktywnie uczestniczymy w procesie wpływania na uświadamianie praw i obowiązków zainteresowanych
+     stron zarówno w konkretnych sytuacjach (ocena interpretacji przepisów prawnych w trakcie sporów sądowych) jak i ciągłe sygnalizowanie (interwencje) odpowiedzialnym instytucjom
+      (organy ścigania, ministerstwa, inspekcje pracy, wojewodowie itp.) konieczności postępowania zgodnie z obowiązującym prawem.</p>
+      </div>
+    </article>
+
+    <article className="flex flex-row">
+    <div className="basis-1/4"></div>
+    <div className="basis-1/2">
+      <h1 className="text-purple-700 font-bold my-4">Cele statutowe stowarzyszenia:</h1>
+      <ul className="list-disc">
+        <li className="text-slate-500">podnoszenie świadomości w zakresie profilaktyki i skutecznej walki z mobbingiem, rozumianym jako przemoc psychiczna w pracy</li>
+        <li className="text-slate-500">nagłaśnianie problemu mobbingu, pokazywanie skutków ekonomicznych, zdrowotnych i moralnych występowania tego zjawiska</li>
+        <li className="text-slate-500">pomoc osobom zagrożonym i poddawanym mobbingowi</li>
+        <li className="text-slate-500">prowadzenie poradnictwa prawnego, psychologicznego i medycznego,
+  prowadzenie działalności mediacyjnej</li>
+        <li className="text-slate-500">działania na rzecz prawa do pracy w warunkach pozbawionych przemocy psychicznej</li>
+        <li className="text-slate-500">współpraca z osobami i instytucjami w zakresie zbierania informacji i wymiany doświadczeń</li>
+        <li className="text-slate-500">działania na rzecz ochrony zdrowia osób doświadczających mobbingu</li>
+        <li className="text-slate-500">przeciwdziałanie wszelkim przejawom dyskryminacji w miejscu pracy oraz działanie na rzecz równego statusu kobiet i mężczyzn</li>
+        <li className="text-slate-500">przeciwdziałanie wykluczeniu społecznemu osób mobbingowanych</li>
+      </ul>
+    </div>
+    </article>
+
+    <article className="flex flex-row">
+    <div className="basis-1/4"></div>
+    <div className="basis-1/2">
+      <h1 className="text-purple-700 font-bold my-4">Realizujemy poprzez:</h1>
+      <ul className="list-disc">
+        <li className="text-slate-500">zrzeszanie w swoich szeregach specjainteresowanych identyfikacji i prewencji przypadków dyskryminacji i przemocy w miejscu pracy</li>
+        <li className="text-slate-500">pomoc prawną, medyczną, psychologiczną</li>
+        <li className="text-slate-500">tworzenie grup samopomocowych i grup wsparcia dla ofiar mobbingu</li>
+        <li className="text-slate-500">działalność wydawniczą i dystrybucyjną materiałów edukacyjnych</li>
+        <li className="text-slate-500">współpracę z mediami</li>
+        <li className="text-slate-500">organizowanie szkoleń w zakresie technik mediacyjnych oraz negocjacyjnych</li>
+        <li className="text-slate-500">rozszerzanie działalności przez tworzenie punktów konsultacyjnych i oddziałów na terenie całego kraju</li>
+        <li className="text-slate-500">podejmowanie prac badawczych, analiz i ekspertyz związanych z mobbingiem i jego skutkami</li>
+        <li className="text-slate-500">szkolenie mediatorów i negocjatorów</li>
+        <li className="text-slate-500">pozyskiwanie sponsorów</li>
+        <li className="text-slate-500">współpracę z innymi organizacjami w kraju i zagranicą</li>
+        <li className="text-slate-500">organizowanie i prowadzenie zjazdów, konferencji, szkoleń i prelekcji</li>
+        <li className="text-slate-500">podejmowanie działań zmierzających do powstania prawnych uregulowań przeciwdziałających mobbingowi</li>
+        <li className="text-slate-500">inicjowanie, opiniowanie i uczestniczenie w pracach legislacyjnych, dotyczących tematyki związanej z działalnością prowadzoną przez stowarzyszenie</li>
+      </ul>
+    </div>
+    </article>
+
+    <article className="flex flex-row">
+    <div className="basis-1/4"></div>
+    <div className="basis-1/2">
+      <h1 className="text-purple-700 font-bold my-4">Warunki wstąpienia do Stowarzyszenia "OSA".</h1>
+      <p className="text-slate-500 my-1">Członkiem Ogólnopolskiego Stowarzyszenia Antymobbingowego "OSA" może zostać każdy, kto :</p>
+      <ul className="list-disc">
+        <li className="text-slate-500">zna i chce przestrzegać postanowień statutu, jak też celów i zadań Stowarzyszenia</li>
+        <li className="text-slate-500">chce uczestniczyć w działalności Stowarzyszenia, pomagać innym członkom Stowarzyszenia w realizowaniu zadań statutowych</li>
+        <li className="text-slate-500">sumiennie wypełnia uchwały władz Stowarzyszenia</li>
+        <li className="text-slate-500">dba o dobry wizerunek Stowarzyszenia i jego członków oraz godnie reprezentuje Stowarzyszenie na zewnątrz</li>
+        <li className="text-slate-500">przeciwdziała zachowaniom mobbingowym</li>
+        <li className="text-slate-500">przestrzega instrukcji obsługi i kontroli dokumentów, która wprowadza jednolite zasady sporządzania , kontroli i obiegu dokumentów w Stowarzyszeniu oraz jednolity mechanizm działania Stowarzyszenia na terenie całego kraju</li>
+        <li className="text-slate-500">regularnie opłaca składki członkowskie w kwocie 5,00 złotych (słownie pięć złotych) miesięcznie</li>
+      </ul>
+    </div>
+    </article>
+
+    <article className="flex flex-row">
+    <div className="basis-1/4"></div>
+    <div className="basis-1/2">
+      <h1 className="text-purple-700 font-bold my-4">Co należy zrobić, zostać członkiem Stowarzyszenia "OSA" ?</h1>
+      <ul className="list-decimal">
+        <li className="text-slate-500">Zgłosić chęć członkostwa do Zarządu Stowarzyszenia lub jego Punktów Konsultacyjnych</li>
+        <li className="text-slate-500">Zapoznać się z treścią ustawy o stowarzyszeniach, statutem Stowarzyszenia, ustawy o ochronie danych osobowych, ustawy o pożytku publicznym i wolontariacie</li>
+        <li className="text-slate-500">Wypełnić deklarację członkowską, na podstawie której podejmowana jest przez Zarząd Stowarzyszenia decyzja o przyjęciu w poczet członków</li>
+        <li className="text-slate-500">Wpłacić jednorazowe wpisowe w kwocie 20,00 złotych(słownie dwadzieścia złotych)</li>
+      </ul>
+    </div>
+    </article>
+
+
      </>
      );
   }
