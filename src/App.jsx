97144c65331391b906d355f12898a63cdb593c34
Łukasz Chwalisz <lukaszchwalisz@proton.me>
2024-03-20 09:41:10 +0100

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import { Navbar } from "./components/navbar";
