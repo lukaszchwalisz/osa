32a5581b44e7a149d3c27f7422ba21333906d13a
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-03-27 14:38:27 +0100

    flex/grid

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,9 +2,9 @@
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col h-screen">
+      <div className="grid justify-center">
           <div className="m-auto"><h1 className="text-slate-500 text-center font-bold text-4xl">STRONA W PRZYGOTOWANIU ! </h1><div className="m-auto"><h1 className="text-slate-400 text-center text-4xl">Prosimy o cierpliwość.</h1></div></div>
-          <img className="scale-75" src="/osa/src/assets/images/1_5ngo.png" alt="logo OSA"></img>
+          <img className="sm:scale-100 md:scale-75 lg:50 xl:25 2xl:25" src="/osa/src/assets/images/1_5ngo.png" alt="logo OSA"></img>
       </div>
     </>   
   );
