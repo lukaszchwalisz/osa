c51104966d8f9d1c72ec8571a66808bbf7da87bf
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-04-09 23:04:51 +0200

    add programy and wydarzenia

--- src/pages/actions.jsx
+++ src/pages/actions.jsx
@@ -8,14 +8,223 @@ const Actions = () => {
 
     <Tabs aria-label="Tabs with underline" style="underline">
       <Tabs.Item active title="Programy">
-        <p className="text-sm text-gray-500 dark:text-gray-400">Content 1</p>
+
+      <article >
+          <h1 className="text-indigo-700 dark:text-indigo-400 font-bold font-mono text-2xl mt-8 mb-4">Realizowany program.</h1>
+          <p className="text-slate-500 dark:text-slate-200 mb-4">
+          Od dnia 10 stycznia 2023r. realizujemy nieodpłatną pomoc prawno - psychologiczną skierowaną do mieszkańców biorących udział w procesie pracy,
+          osób zagrożonych i dotkniętych zjawiskiem mobbingu oraz dyskryminacji, opartą na przesłankach takich jak: bezpłatność, bezstronność, otwartość
+          dla wszystkich, poufność, niezależność, aktualność i rzetelność. Finansowaną ze środków własnych stowarzyszenia oraz dofinansowania ze środków
+          Miasta Gdańsk w ramach Gdańskiego Funduszu Równości 2023.
+          </p>
+
+          <ul className="list-decimal">
+            <li className="text-slate-500 dark:text-slate-200 mb-4">
+            <b>Pomoc prawna polega na wyjaśnianiu wątpliwości prawnych osoby zgłaszającej się w przedstawionej przez nią sprawie:</b>
+            </li>
+
+            <ul className="list-disc">
+              <li className="text-slate-500 dark:text-slate-200 mb-4">
+              udzieleniu odpowiedzi na pytania związane z obowiązującym prawem w zakresie wiążącym się ze
+              sprawą tj. udzieleniu informacji i edukacji o treści obowiązującego prawa, o prawach i 
+              obowiązkach wynikających z obowiązujących uregulowań prawnych oraz instytucjach właściwych dla
+              załatwienia sprawy a także o trybie postępowania
+              </li>
+              <li className="text-slate-500 dark:text-slate-200">
+              wskazaniu możliwych prawnie dróg postępowania
+              </li>
+              <p className="text-slate-500 dark:text-slate-200">
+              Pomoc udzielana jest w kontakcie bezpośrednim, telefonicznie,
+              pocztą internetową oraz pocztą tradycyjną poprzez:
+              </p>
+              <li className="text-slate-500 dark:text-slate-200">
+              udzielanie indywidualnych konsultacji osobom zwracającym się o pomoc
+              </li>
+              <li className="text-slate-500 dark:text-slate-200">
+              ocenę faktów związanych z przemocą w pracy z uwzględnieniem kwalifikacji prawnej problemów
+              </li>
+              <li className="text-slate-500 dark:text-slate-200">
+              ocenę realnych szans powodzenia postępowań sądowych w sprawach z zakresu mobbingu i dyskryminacji (ocena mocy dowodów
+              </li>
+              <li className="text-slate-500 dark:text-slate-200">
+              pomoc w formułowaniu treści dokumentów
+              </li>
+              <li className="text-slate-500 dark:text-slate-200 mb-4">
+              w razie potrzeby w postępowaniu sądowym prawnik funkcjonuje jako pełnomocnik Stowarzyszenia, organizacji społecznej broniącej praw człowieka,
+              jeśli problem zostanie zakwalifikowany do tego typu pomocy
+              </li>
+              <p className="text-slate-500 dark:text-slate-200 mb-4">
+              Pomoc prowadzi Konsultant Krajowy Stowarzyszenia, pani mgr Jadwiga Mucha.
+              </p>
+
+              <ul className="list-none">
+                <li className="text-slate-500 dark:text-slate-200">
+                tel.: 504 659 911 (w dni powszednie w godzinach 18.00 - 20.00)
+                </li>
+                <li className="text-slate-500 dark:text-slate-200 mb-4">
+                <a href="mailto:osa-lublin@wp.pl">email.: osa-lublin@wp.pl</a>
+                </li>
+              </ul>
+              </ul>
+              
+              <li className="text-slate-500 dark:text-slate-200 mb-4">
+              <b>Pomoc psychologiczna realizowana w formie społecznego wsparcia grupowego w otwartej grupie wsparcia prowadzonej przez psychologów mgr Barbarę Jabłońską - Komar i mgr Paulinę Pers,
+              gdzie realizowany jest program psychoprofilaktyczny "Pracuję bez lęku" o tematyce zajęć dostosowanej do zapotrzebowania uczestników t.j.:</b>
+              </li>
+
+              <ul className="list-disc">
+                <li className="text-slate-500 dark:text-slate-200">
+                podstawowe informacje o mobbingu i dyskryminacji. Czym jest dyskryminacja i mobbing, różnice? Czynniki występowania dyskryminacji i mobbingu. Fazy mobbingu.
+                </li>
+                <li className="text-slate-500 dark:text-slate-200">
+                skutki w/w zjawisk: zdrowotne (nerwobóle, choroba wrzodowa itd.);psychologiczne (stres w pracy); motywacyjne(wypalenie zawodowe)
+                </li>
+                <li className="text-slate-500 dark:text-slate-200">
+                diagnoza sytuacji dyskryminacyjnej i mobbingowej: relacja pracodawca/przełożony (analiza transakcyjna); konflikt w pracy; komunikacja w pracy
+                </li>
+                <li className="text-slate-500 dark:text-slate-200">
+                moja postawa wobec dyskryminacji i mobbingu: postawa agresywna; postawa uległa; postawa asertywna; pozytywne praktyczne wzorce
+                </li>
+                <li className="text-slate-500 dark:text-slate-200 mb-4">
+                skuteczne sposoby walki z dyskryminacją i mobbingiem: autoprezentacja; poczucie własnej wartości; negocjacje/ mediacje; konflikt; radzenie sobie ze stresem; zarządzanie czasem
+                </li>
+              </ul>
+              <p className="text-slate-500 dark:text-slate-200 mb-4">
+              W trakcie zajęć można:
+              </p>
+              <ul className="list-disc">
+                <li className="text-slate-500 dark:text-slate-200">
+                otrzymać wsparcie i pomoc od osób rozumiejących ten problem
+                </li>
+                <li className="text-slate-500 dark:text-slate-200">
+                spotkać osoby o podobnym doświadczeniu
+                </li>
+                <li className="text-slate-500 dark:text-slate-200">
+                uzyskać wiedzę o mechanizmach, konsekwencjach przemocy w pracy
+                </li>
+                <li className="text-slate-500 dark:text-slate-200">
+                uzyskać diagnozę sytuacji
+                </li>
+                <li className="text-slate-500 dark:text-slate-200">
+                uzyskać pomoc w rozwiązywaniu problemów emocjonalnych
+                </li>
+                <li className="text-slate-500 dark:text-slate-200">
+                uzyskać pomoc w odzyskaniu utraconych praw
+                </li>
+                <li className="text-slate-500 dark:text-slate-200 mb-4">
+                uzyskać motywację do aktywnego uczestnictwa w grupie i udzielanie wzajemnego wsparcia z 
+                wykorzystaniem własnej wiedzy i umiejętności
+                </li>
+              </ul>
+
+              <p className="text-slate-500 dark:text-slate-200 mb-4">
+              Zajęcia odbywają się we wtorki od 10 stycznia 2023r w godzinach od 17.00 do 19.00 w pomieszczeniach
+              użyczonych odpłatnie przez Towarzystwo Przyjaciół Gdańska w Gdańsku przy ul. Warzywniczej 10.
+              </p>
+              <p className="text-slate-500 dark:text-slate-200 mb-4">
+              W okresie od 1 czerwca do 8 grudnia 2023r. zajęcia odbywają się dzięki dofinansowaniu ze środków Miasta Gdańsk
+              w ramach Gdańskiego Funduszu Równości 2023.
+              </p>
+              <p className="text-slate-500 dark:text-slate-200 mb-4">
+              Serdecznie zapraszamy do uczestnictwa :)
+              </p>
+              <p className="text-slate-500 dark:text-slate-200 mb-4">
+              Konsultacje indywidualne w Punkcie Konsultacyjnym Zarządu Stowarzyszenia w Gdańsku mieszczącym się w Gdańsku przy ul. Warzywniczej 10A
+              (domofon nr 6) odbywają się we wtorki od godz. 16.00 po uprzednim koniecznym umówieniu się telefonicznym: 795 159 795.
+              </p>
+          </ul>
+
+      </article>
+
+      
       </Tabs.Item>
       <Tabs.Item title="Komunikaty">
         <p className="text-sm text-gray-500 dark:text-gray-400">Content 2</p>
       </Tabs.Item>
+
       <Tabs.Item title="Wydarzenia">
-        <p className="text-sm text-gray-500 dark:text-gray-400">Content 3</p>
+       
+     
+        <article >
+          <h1 className="text-indigo-700 dark:text-indigo-400 font-bold font-mono text-2xl mt-8 mb-4">Wydarzenia.</h1>
+         
+          <ul className="list-disc">
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 9 października 2017r. przedstawiciel Zarządu Stowarzyszenia Mariola Żarnoch uczestniczyła w IX Ogólnopolskim Kongresie Kobiet
+            pod hasłem „Alert dla praw kobiet!” w Poznaniu, jako panelista – konsultant w panelu pt. „M jak mobbing. O prześladowaniu dyskryminacji w szkołach,
+            na uczelniach i w instytucjach naukowych z perspektywy kobiet”.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 18 czerwca 2015r. członkowie Punktu Konsultacyjnego Zarządu OSA w Lubaniu wraz z zarządem stowarzyszenia zorganizowali i przeprowadzili
+            konferencję - „Mobbing się nie opłaca”, w której uczestniczyły 43 osoby.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniach 27-28 maja 2015r. stowarzyszenie uczestniczyło, jako prelegent w obradach panelowych w I Ogólnopolskiej Interdyscyplinarnej Konferencji Naukowej
+            pt. Patologie Współczesnego świata. Przemoc” organizowanej przez Uniwersytet Gdański Wydział Nauk Społecznych Instytut Politologii w Gdańsku, 
+            Politechnikę Gdańską Wydział Zarządzania i Ekonomii oraz CKZiU1- Ośrodek Kształcenia Ustawicznego Nauczycieli.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 16 października 2014r. członkowie Punktu Konsultacyjnego Zarządu OSA w Olsztynie wraz z Zarządem stowarzyszenia zorganizowali konferencję
+            – „ Przeciwdziałaj mobbingowi – lepiej zapobiegać” , w której uczestniczyło 200 osób.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 9 października 2013r. stowarzyszenie uczestniczyło jako prelegent w seminarium w ramach Kampanii Państwowej Inspekcji Pracy „Bezpieczeństwo Pracy zależy od ciebie”
+            organizowanym przez Ogólnopolskie Stowarzyszenie Pracowników Służby BHP w Gdańsku. Rozdano uczestnikom 130 kpl. materiałów edukacyjnych wytworzonych przez stowarzyszenie.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 19 marca 2007r. członkowie Punktu Konsultacyjnego Zarządu OSA w Jeleniej Górze pod patronatem Burmistrza Piechowic zrealizowali program pt. 
+            „Mobbing a budowa poprawnych stosunków pracy w oświacie” w postaci środowiskowej konferencji, która odbyła się w Ośrodku Kultury w Piechowicach.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W okresie od 5 września 2006r. do 6 października 2006r. Stowarzyszenie uczestniczyło w kampanii Stop-mobbing w Gorzowie Wielkopolskim. Przedstawiciel Zarządu na woj. lubuskie
+            Elżbieta Król uczestniczyła w warsztatach i konferencjach organizowanych w ramach kampanii, a w dniu 26.09.2006 w Państwowej Inspekcji Pracy przy ul. Mościckiego 3 w godzinach 9-14
+            udzielała porad dla mieszkańców Gorzowa Wielkopolskiego. W ramach kampanii umieszczono link do strony Ogólnopolskiego Stowarzyszenia Antymobbingowego OSA na stronie internetowej
+            przygotowanej przez Urząd Miasta - www.stop-mobbing.gorzow.pl
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 26 listopada 2005r. zorganizowana została przez członków Punktu Konsultacyjnego Zarządu w Raciborzu wraz z Zarządem Stowarzyszenia konferencja - 
+            "Mobbing - klęska zarządzania czy problem społeczny ?" pod patronatem honorowym Rzecznika Praw Obywatelskich i Pełnomocnika Wojewody Śląskiego ds. Równego Statusu Kobiet i Mężczyzn.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 12 marca 2005r. stowarzyszenie jako współpracownik merytoryczny uczestniczyło w zorganizowaniu Ogólnopolskiej Konferencji pt. „Mobbing w Oświacie”, która odbyła się w Tarnowskich Górach.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 20 listopada 2004r. członkowie Punktu Konsultacyjnego Zarządu OSA w Jeleniej Górze wspólnie z Komisją Międzyzakładową NSZZ Solidarność Pracowników Oświaty i Wychowania zorganizowali
+             Dyskusyjne Forum Antymobbingowe z udziałem przedstawicieli kluczowych instytucji i organizacji Jeleniej Góry.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 29 czerwca 2004r. w Gdańsku zorganizowana została przez zarząd stowarzyszenia konferencja - "Mobbing - zalążek klęski czy sukcesu?" współfinansowana ze środków Unii Europejskiej
+            w ramach Counterpart Funds.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 16 marca 2004r. stowarzyszenie uczestniczyło w charakterze prelegenta na konferencji: „Konwent HR - Nowelizacja Prawa Pracy w Praktyce”, organizowanej przez Polskie Stowarzyszenie
+            Zarządzania Kadrami w Hotelu Sheraton w Warszawie. Otrzymaliśmy wysokie notowania uczestników. Materiały po konferencyjne naszego wystąpienia zostały przesłane celem opublikowania na stronie internetowej
+            Stowarzyszenia organizującego konferencję.
+            </li>
+            <li className="text-slate-500 dark:text-slate-200">
+            W dniu 17 października 2003r. na rozprawie przeciwko Wojewódzkiej Stacji Sanitarno Epidemiologicznej w Białymstoku Sąd Pracy i Ubezpieczeń Społecznych Sądu Rejonowego w Białymstoku
+            dopuścił nasze stowarzyszenie do toczącego się tam postępowania, jako pierwszą tego typu w Polsce organizację społeczną działającą w zakresie obrony praw człowieka. 
+            Wstąpienie Stowarzyszenia do procesu zostało uznane za precedens. Nigdy wcześniej, pomimo toczących się wielu spraw o mobbing w Polsce, żadne ze stowarzyszeń nie poparło powództwa
+            w postępowaniu sądowym, a mobbing nie miał jeszcze swej podstawy prawnej w ówcześnie obowiązujących przepisach prawa.
+            </li>
+
+                <b>Add timeline and links.</b>
+          </ul>
+         
+         
+          <p className="text-slate-500 dark:text-slate-200 mb-4"></p>
+     
+     
+     
+     </article>
+     
+     
+     
       </Tabs.Item>
+
+
+
+
       <Tabs.Item title="Archiwum">
         <p className="text-sm text-gray-500 dark:text-gray-400">Content 4</p>
       </Tabs.Item>
