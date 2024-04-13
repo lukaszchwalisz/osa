acffcfb771e4c71822ba9a9caf8acdb729afeeca
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-04-13 21:08:46 +0200

    testing pdfs display

--- src/pages/actions.jsx
+++ src/pages/actions.jsx
@@ -642,20 +642,28 @@ const Actions = () => {
          
          
           <p className="text-slate-500 dark:text-slate-200 mb-4"></p>
-     
-     
-     
      </article>
-     
-     
-     
+    
       </Tabs.Item>
 
 
 
 
       <Tabs.Item title="Archiwum">
+      <article >
       <h1 className="text-indigo-700 bg-slate-50 p-4 text-center dark:text-indigo-400 font-bold font-mono text-2xl mt-8 mb-4">ARCHIWUM</h1>
+      <p className="text-slate-500 dark:text-slate-200 mb-4">
+      Programy i projekty stworzone i zrealizowane przez Ogólnopolskie Stowarzyszenie Antymobbingowe OSA (pliki pdf):
+      </p>
+      
+      <ul className="list-disc">
+              
+        <li className="text-slate-500 dark:text-slate-200 m-3"><a href="/osa/src/assets/pdf/projekty.pdf" target="_blank">Projekty</a></li>
+              
+      </ul>
+      
+      
+      </article>
       </Tabs.Item>
     </Tabs>
 
--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -16,6 +16,7 @@ const Kontakt = () => {
           <li className="text-slate-500 dark:text-slate-200">kom.: 795 159 795</li>
           <li className="text-slate-500 dark:text-slate-200">email: <a href="mailto:osa_stowarzyszenie@wp.pl">osa_stowarzyszenie@wp.pl</a></li>
           <li className="text-slate-500 dark:text-slate-200">www: <a href="http://www.osastowarzyszenie.pl/start.html">http://www.osastowarzyszenie.pl/</a></li>
+          <li className="text-slate-500 dark:text-slate-200">www: <a href="http://www.osastowarzyszenie.pl/poznan.pdf" target="_blank">pdf</a></li>
       </ul>
       </p>
 
