9ff98ec8c2169983f4cc3c430459efa472b68cef
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-04-17 15:58:28 +0200

    test flowbite list version

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import { Link } from "react-router-dom"
-import { Tabs } from "flowbite-react"
+import { List, Tabs } from "flowbite-react"
+
 
 const About = () => {
   return (
@@ -91,72 +92,76 @@ const About = () => {
         </article>
       </Tabs.Item>
       <Tabs.Item title="Cele statutowe">
-            <article>
 
-              <h1 className="text-indigo-700 dark:text-indigo-400 font-bold font-mono text-2xl mt-8 mb-4">Cele statutowe stowarzyszenia:</h1>
-              <ul className="list-disc">
-                <li className="text-slate-500 dark:text-slate-200">podnoszenie świadomości w zakresie profilaktyki i skutecznej walki z mobbingiem, rozumianym jako przemoc psychiczna w pracy</li>
-                <li className="text-slate-500 dark:text-slate-200">nagłaśnianie problemu mobbingu, pokazywanie skutków ekonomicznych, zdrowotnych i moralnych występowania tego zjawiska</li>
-                <li className="text-slate-500 dark:text-slate-200">pomoc osobom zagrożonym i poddawanym mobbingowi</li>
-                <li className="text-slate-500 dark:text-slate-200">prowadzenie poradnictwa prawnego, psychologicznego i medycznego,
-              prowadzenie działalności mediacyjnej</li>
-                <li className="text-slate-500 dark:text-slate-200">działania na rzecz prawa do pracy w warunkach pozbawionych przemocy psychicznej</li>
-                <li className="text-slate-500 dark:text-slate-200">współpraca z osobami i instytucjami w zakresie zbierania informacji i wymiany doświadczeń</li>
-                <li className="text-slate-500 dark:text-slate-200">działania na rzecz ochrony zdrowia osób doświadczających mobbingu</li>
-                <li className="text-slate-500 dark:text-slate-200">przeciwdziałanie wszelkim przejawom dyskryminacji w miejscu pracy oraz działanie na rzecz równego statusu kobiet i mężczyzn</li>
-                <li className="text-slate-500 dark:text-slate-200 mb-4">przeciwdziałanie wykluczeniu społecznemu osób mobbingowanych</li>
-              </ul>
+          <article>
 
-              <p className="text-slate-500 dark:text-slate-200 font-bold underline mb-4">Realizujemy poprzez:</p>
-              <ul className="list-disc">
-                <li className="text-slate-500 dark:text-slate-200">zrzeszanie w swoich szeregach specjainteresowanych identyfikacji i prewencji przypadków dyskryminacji i przemocy w miejscu pracy</li>
-                <li className="text-slate-500 dark:text-slate-200">pomoc prawną, medyczną, psychologiczną</li>
-                <li className="text-slate-500 dark:text-slate-200">tworzenie grup samopomocowych i grup wsparcia dla ofiar mobbingu</li>
-                <li className="text-slate-500 dark:text-slate-200">działalność wydawniczą i dystrybucyjną materiałów edukacyjnych</li>
-                <li className="text-slate-500 dark:text-slate-200">współpracę z mediami</li>
-                <li className="text-slate-500 dark:text-slate-200">organizowanie szkoleń w zakresie technik mediacyjnych oraz negocjacyjnych</li>
-                <li className="text-slate-500 dark:text-slate-200">rozszerzanie działalności przez tworzenie punktów konsultacyjnych i oddziałów na terenie całego kraju</li>
-                <li className="text-slate-500 dark:text-slate-200">podejmowanie prac badawczych, analiz i ekspertyz związanych z mobbingiem i jego skutkami</li>
-                <li className="text-slate-500 dark:text-slate-200">szkolenie mediatorów i negocjatorów</li>
-                <li className="text-slate-500 dark:text-slate-200">pozyskiwanie sponsorów</li>
-                <li className="text-slate-500 dark:text-slate-200">współpracę z innymi organizacjami w kraju i zagranicą</li>
-                <li className="text-slate-500 dark:text-slate-200">organizowanie i prowadzenie zjazdów, konferencji, szkoleń i prelekcji</li>
-                <li className="text-slate-500 dark:text-slate-200">podejmowanie działań zmierzających do powstania prawnych uregulowań przeciwdziałających mobbingowi</li>
-                <li className="text-slate-500 dark:text-slate-200 mb-4">inicjowanie, opiniowanie i uczestniczenie w pracach legislacyjnych, dotyczących tematyki związanej z działalnością prowadzoną przez stowarzyszenie</li>
-              </ul>
+            <h1 className="text-indigo-700 dark:text-indigo-400 font-bold font-mono text-2xl mt-8 mb-4">Cele statutowe stowarzyszenia:</h1>
 
-      </article>
+            <List className="text-slate-500 dark:text-slate-200 mb-4">
+              <List.Item>podnoszenie świadomości w zakresie profilaktyki i skutecznej walki z mobbingiem, rozumianym jako przemoc psychiczna w pracy</List.Item>
+              <List.Item>nagłaśnianie problemu mobbingu, pokazywanie skutków ekonomicznych, zdrowotnych i moralnych występowania tego zjawiska</List.Item>
+              <List.Item>pomoc osobom zagrożonym i poddawanym mobbingowi</List.Item>
+              <List.Item>prowadzenie poradnictwa prawnego, psychologicznego i medycznego, prowadzenie działalności mediacyjnej</List.Item>
+              <List.Item>działania na rzecz prawa do pracy w warunkach pozbawionych przemocy psychicznej</List.Item>
+              <List.Item>współpraca z osobami i instytucjami w zakresie zbierania informacji i wymiany doświadczeń</List.Item>
+              <List.Item>działania na rzecz ochrony zdrowia osób doświadczających mobbingu</List.Item>
+              <List.Item>przeciwdziałanie wszelkim przejawom dyskryminacji w miejscu pracy oraz działanie na rzecz równego statusu kobiet i mężczyzn</List.Item>
+              <List.Item>przeciwdziałanie wykluczeniu społecznemu osób mobbingowanych</List.Item>
+            </List>
+
+            <p className="text-slate-500 dark:text-slate-200 font-bold underline mb-4">Realizujemy poprzez:</p>
+
+            <List className="text-slate-500 dark:text-slate-200 mb-4">
+              <List.Item>zrzeszanie w swoich szeregach specjainteresowanych identyfikacji i prewencji przypadków dyskryminacji i przemocy w miejscu pracy</List.Item>
+              <List.Item>pomoc prawną, medyczną, psychologiczną</List.Item>
+              <List.Item>tworzenie grup samopomocowych i grup wsparcia dla ofiar mobbingu</List.Item>
+              <List.Item>działalność wydawniczą i dystrybucyjną materiałów edukacyjnych</List.Item>
+              <List.Item>współpracę z mediami</List.Item>
+              <List.Item>organizowanie szkoleń w zakresie technik mediacyjnych oraz negocjacyjnych</List.Item>
+              <List.Item>rozszerzanie działalności przez tworzenie punktów konsultacyjnych i oddziałów na terenie całego kraju</List.Item>
+              <List.Item>podejmowanie prac badawczych, analiz i ekspertyz związanych z mobbingiem i jego skutkami</List.Item>
+              <List.Item>szkolenie mediatorów i negocjatorów</List.Item>
+              <List.Item>pozyskiwanie sponsorów</List.Item>
+              <List.Item>współpracę z innymi organizacjami w kraju i zagranicą</List.Item>
+              <List.Item>organizowanie i prowadzenie zjazdów, konferencji, szkoleń i prelekcji</List.Item>
+              <List.Item>podejmowanie działań zmierzających do powstania prawnych uregulowań przeciwdziałających mobbingowi</List.Item>
+              <List.Item>inicjowanie, opiniowanie i uczestniczenie w pracach legislacyjnych, dotyczących tematyki związanej z działalnością prowadzoną przez stowarzyszenie</List.Item>
+            </List>
+
+          </article>
  
       </Tabs.Item>
-      <Tabs.Item title="Jak dołączyć ?">
 
-        <article>
+        <Tabs.Item title="Jak dołączyć ?">
 
-          <h1 className="text-indigo-700 dark:text-indigo-400 font-bold font-mono text-2xl mt-8 mb-4">Warunki wstąpienia do Stowarzyszenia "OSA".</h1>
-          <p className="text-slate-500 dark:text-slate-200 mb-4 my-1">Członkiem Ogólnopolskiego Stowarzyszenia Antymobbingowego "OSA" może zostać każdy, kto :</p>
-          <ul className="list-disc">
-            <li className="text-slate-500 dark:text-slate-200">zna i chce przestrzegać postanowień statutu, jak też celów i zadań Stowarzyszenia</li>
-            <li className="text-slate-500 dark:text-slate-200">chce uczestniczyć w działalności Stowarzyszenia, pomagać innym członkom Stowarzyszenia w realizowaniu zadań statutowych</li>
-            <li className="text-slate-500 dark:text-slate-200">sumiennie wypełnia uchwały władz Stowarzyszenia</li>
-            <li className="text-slate-500 dark:text-slate-200">dba o dobry wizerunek Stowarzyszenia i jego członków oraz godnie reprezentuje Stowarzyszenie na zewnątrz</li>
-            <li className="text-slate-500 dark:text-slate-200">przeciwdziała zachowaniom mobbingowym</li>
-            <li className="text-slate-500 dark:text-slate-200">przestrzega instrukcji obsługi i kontroli dokumentów, która wprowadza jednolite zasady sporządzania , kontroli i obiegu dokumentów w Stowarzyszeniu oraz jednolity mechanizm działania Stowarzyszenia na terenie całego kraju</li>
-            <li className="text-slate-500 dark:text-slate-200 mb-4">regularnie opłaca składki członkowskie w kwocie 5,00 złotych (słownie pięć złotych) miesięcznie</li>
-          </ul>
+            <article>
+              <h1 className="text-indigo-700 dark:text-indigo-400 font-bold font-mono text-2xl mt-8 mb-4">Warunki wstąpienia do Stowarzyszenia "OSA".</h1>
+              
+              <p className="text-slate-500 dark:text-slate-200 mb-4 my-1">Członkiem Ogólnopolskiego Stowarzyszenia Antymobbingowego "OSA" może zostać każdy, kto :</p>
+            
+              <List className="text-slate-500 dark:text-slate-200 mb-4">
+                <List.Item>zna i chce przestrzegać postanowień statutu, jak też celów i zadań Stowarzyszenia</List.Item>
+                <List.Item>chce uczestniczyć w działalności Stowarzyszenia, pomagać innym członkom Stowarzyszenia w realizowaniu zadań statutowych</List.Item>
+                <List.Item>sumiennie wypełnia uchwały władz Stowarzyszenia</List.Item>
+                <List.Item>dba o dobry wizerunek Stowarzyszenia i jego członków oraz godnie reprezentuje Stowarzyszenie na zewnątrz</List.Item>
+                <List.Item>przeciwdziała zachowaniom mobbingowym</List.Item>
+                <List.Item>przestrzega instrukcji obsługi i kontroli dokumentów, która wprowadza jednolite zasady sporządzania, kontroli i obiegu dokumentów w Stowarzyszeniu oraz jednolity mechanizm działania Stowarzyszenia na terenie całego kraju</List.Item>
+                <List.Item>regularnie opłaca składki członkowskie w kwocie 5,00 złotych (słownie pięć złotych) miesięcznie</List.Item>
+              </List>
+      
+              <p className="text-slate-500 dark:text-slate-200 font-bold underline mb-4">Co należy zrobić, zostać członkiem Stowarzyszenia "OSA" ?</p>
 
-          <p className="text-slate-500 dark:text-slate-200 font-bold underline mb-4">Co należy zrobić, zostać członkiem Stowarzyszenia "OSA" ?</p>
-          <ul className="list-decimal">
-            <li className="text-slate-500 dark:text-slate-200">Zgłosić chęć członkostwa do Zarządu Stowarzyszenia lub jego Punktów Konsultacyjnych</li>
-            <li className="text-slate-500 dark:text-slate-200">Zapoznać się z treścią ustawy o stowarzyszeniach, statutem Stowarzyszenia, ustawy o ochronie danych osobowych, ustawy o pożytku publicznym i wolontariacie</li>
-            <li className="text-slate-500 dark:text-slate-200">Wypełnić deklarację członkowską, na podstawie której podejmowana jest przez Zarząd Stowarzyszenia decyzja o przyjęciu w poczet członków</li>
-            <li className="text-slate-500 dark:text-slate-200 mb-4">Wpłacić jednorazowe wpisowe w kwocie 20,00 złotych(słownie dwadzieścia złotych)</li>
-          </ul>
-          </article>
+              <List ordered className="text-slate-500 dark:text-slate-200">
+                <List.Item>Zgłosić chęć członkostwa do Zarządu Stowarzyszenia lub jego Punktów Konsultacyjnych.</List.Item>
+                <List.Item>Zapoznać się z treścią ustawy o stowarzyszeniach, statutem Stowarzyszenia, ustawy o ochronie danych osobowych, ustawy o pożytku publicznym i wolontariacie.</List.Item>
+                <List.Item>Wypełnić deklarację członkowską, na podstawie której podejmowana jest przez Zarząd Stowarzyszenia decyzja o przyjęciu w poczet członków.</List.Item>
+                <List.Item>Wpłacić jednorazowe wpisowe w kwocie 20,00 złotych(słownie dwadzieścia złotych).</List.Item>
+              </List>
+            </article>
+
+        </Tabs.Item>
 
 
-
-
-      </Tabs.Item>
       <Tabs.Item title="To już 20 lat stowarzyszenia OSA !">
       <h1 className="text-indigo-700 dark:text-indigo-400 font-bold font-mono text-2xl mt-8 mb-4">Historia powstania stowarzyszenia "OSA".</h1>
 
