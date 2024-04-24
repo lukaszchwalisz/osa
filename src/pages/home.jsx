ae132cda67591ddc61432a5a9a9873958bc82b6a
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-04-24 20:21:15 +0200

    small change

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,8 @@ const Home = () => {
   return (
     <>
       <div className="grid justify-center sm:mx-0 md:mx-0 lg:mx-20 xl:mx-40 2xl:mx-80">
-          <div className="m-auto"><h1 className="text-slate-500 text-center font-bold text-4xl">STRONA W PRZYGOTOWANIU ! </h1><div className="m-auto"><h1 className="text-slate-400 text-center text-4xl">Prosimy o cierpliwość.</h1></div></div>
+          <div className="m-auto"><h1 className="text-slate-500 text-center font-bold text-4xl">STRONA W PRZYGOTOWANIU !</h1>
+          <div className="m-auto"><h1 className="text-slate-400 text-center text-4xl">Prosimy o cierpliwość.</h1></div></div>
           <img className="sm:scale-100 md:scale-75 lg:50 xl:25 2xl:25" src="/osa/images/1_5ngo.png" alt="1,5% podatku dla OSA"></img>
       </div>
     </>   
