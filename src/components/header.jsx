51328fdb5c77e7614f2719b636cd177d86218794
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-04-28 18:43:20 +0200

    correct header display

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,9 +7,9 @@ export function Header() {
         <>
         <nav className="sticky top-0 z-10 h-max max-w-full"> 
             <div className="bg-white dark:bg-gradient-to-r from-sky-100 from-10% to-indigo-200 to-90%">
-            <ul  className="container mx-auto sm:grid md:flex flex-row">
+            <ul  className="container mx-auto text-center sm:justify-center sm:items-center sm:grid md:flex flex-row">
                 <span className="flex flex-center flex-1"><li><Link to="/osa"><img className="w-64 m-4 sm:grid" src="/osa/images/Web_logo.svg" alt="logo OSA"></img></Link></li></span>
-                <li className="text-slate-500 font-bold font-mono space-x-2 p-4">
+                <li className="text-slate-500 space-x-2 p-4">
                 <Link to="/osa"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg></Link>
