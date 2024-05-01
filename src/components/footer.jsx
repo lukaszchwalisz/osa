691e68f3bf58bc48cdd6f916d9ad2307c222b05a
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-05-01 16:28:15 +0200

    testing pastels

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@
 
 export function Footer() {
   return (
-    <div className="h-36 p-8 text-center bg-stone-50 dark:text-stone-50 dark:bg-gray-800">
+    <div className="h-36 p-8 text-center bg-lime-100 dark:text-stone-50 dark:bg-gray-800">
       © Ogólnopolskie Stowarzyszenie Antymobbingowe "OSA" 2024
       </div>
 
