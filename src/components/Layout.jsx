4120eb8594675a2d949ed1bdcf0d0a76d033e522
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-05-03 13:54:12 +0200

    footer flex

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -31,6 +31,7 @@ function Layout() {
               <div className="mx-auto w-full max-w-screen-xl">
 
                 <div className="px-4 py-6 md:flex md:items-center md:justify-between">
+
                   <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© Ogólnopolskie Stowarzyszenie Antymobbingowe "OSA", 2024
                   </span>
 
@@ -40,7 +41,9 @@ function Layout() {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
                     </svg>
                   </div>
+
                 </div>
+                
               </div>
 
             </div>
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,11 +8,10 @@ export function Footer() {
 
       <div className="mx-auto w-full max-w-screen-xl">
 
-        <div className="px-4 py-6 md:flex md:items-center">
+        <div className="px-4 py-6 flex md:items-center">
           <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">Sprawdź nas na:
           </span>
-
-          <a href="https://www.facebook.com/osamobbing" target="_blank" className="text-gray-400 m-3 hover:text-gray-900 dark:hover:text-white">
+          <a href="https://www.facebook.com/osamobbing" target="_blank" className="text-gray-400 hover:text-gray-900 mx-2 dark:hover:text-white">
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
               <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
             </svg>
