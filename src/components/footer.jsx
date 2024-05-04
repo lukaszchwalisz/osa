3ddf9458f10f70f8f1f7cc106577676d1b9bb412
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-05-04 15:22:31 +0200

    fixed scroll & Fb

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,18 +7,12 @@ export function Footer() {
     <footer className="bg-slate-50 dark:bg-gray-800">
 
       <div className="mx-auto w-full max-w-screen-xl">
+        <div className="px-6 py-10 md:flex md:items-center md:justify-between">
 
-        <div className="px-4 py-6 flex md:items-center">
-          <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">Sprawdź nas na:
-          </span>
-          <a href="https://www.facebook.com/osamobbing" target="_blank" className="text-gray-400 hover:text-gray-900 mx-2 dark:hover:text-white">
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-              <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
-            </svg>
-            <span className="sr-only">OSA na Fb</span>
-          </a>
+        <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© Ogólnopolskie Stowarzyszenie Antymobbingowe "OSA", 2024
+        </span>
 
-        </div>
+        </div>   
       </div>
 
     </footer>
