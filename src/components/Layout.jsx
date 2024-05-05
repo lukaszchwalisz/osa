7dd02a413e3396199c64de64f2b3e9d8bc1e3ec8
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-05-05 15:57:26 +0200

    add scrolltotop button

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import { Outlet, ScrollRestoration } from "react-router-dom"
+import ScrollToTop from "react-scroll-to-top";
 import { Header } from "./header"
 import { Footer } from "./footer"
 import { useState } from "react";
@@ -44,13 +45,24 @@ function Layout() {
                 <span className="sr-only">OSA na Fb</span>
               </a>
             </div>
-          
-          
-
+    
           </div>
         </div>
-        <ScrollRestoration />
-      </div>
+        <ScrollRestoration smooth/>
+        <ScrollToTop
+        smooth
+        bgcolor="#000"
+        color="#9ca3af"
+        top="20"
+        viewBox="0 0 15 24"
+        className="text-center"
+        svgPath="M4 2C3.44772 2 3 2.44772 3 3C3 3.55228 3.44772 4 4 4H20C20.5523 4 21 3.55228 21 3C21 2.44772 20.5523 2 20 2H4Z 
+        M6.29289 13.7071C6.68342 14.0976 7.31658 14.0976 7.70711 13.7071L11 10.4142L11 21C11 21.5523 11.4477 22 12 22C12.5523 22 13 21.5523 13 21L13 10.4142L16.2929 13.7071C16.6834 14.0976 17.3166 14.0976 17.7071 13.7071C18.0976 13.3166 18.0976 12.6834 17.7071 12.2929L12.7071 7.29289C12.3166 6.90237 11.6834 6.90237 11.2929 7.29289L6.29289 12.2929C5.90237 12.6834 5.90237 13.3166 6.29289 13.7071Z" fill="#000000"
+        
+        />
+        </div>
+      
+
     </>
   )
 }
