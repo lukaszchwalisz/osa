23491c68b69e8d3328269daa359591dfcffa147d
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-05-08 22:14:54 +0200

    try emailjs

---
+++ src/emails/email.jsx
@@ -0,0 +1,35 @@
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const Email = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_2e4ns4j', 'template_51r7uri', form.current, {
+        publicKey: 'cdCD3-UsUijWdhOTZ',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
+import { Email } from "../emails/email"
+
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <div className="grid justify-center m-4 mt-20 mb-20 lg:mx-20 xl:mx-40 2xl:mx-80">
+      <Email />
+    </div>
   )
 }
 
