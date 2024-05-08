d16b4487c7f01a97aaebf6f7a5b3f1cb3e137433
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-05-08 10:38:22 +0200

    layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import Layout from './components/Layout.jsx'
+import Layout from './components/layout.jsx'
 
 import Home from "./pages/home";
 import About from "./pages/about";
