b608d0be35044c64bb7101254af288e33ec415fc
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-05-14 12:37:07 +0200

    create mail form

--- src/emails/email.jsx
+++ src/emails/email.jsx
@@ -7,7 +7,7 @@ export const Email = () => {
   const [message, setMessage] = useState('');
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(false);
 
     // Your EmailJS service ID, template ID, and Public Key
     const serviceId = 'service_2e4ns4j';
@@ -16,9 +16,9 @@ export const Email = () => {
 
     // Create a new object that contains dynamic template params
     const templateParams = {
+      to_name: 'Stowarzyszenie "OSA"',
       from_name: name,
       from_email: email,
-      to_name: 'Stowarzyszenie "OSA"',
       message: message,
     };
 
@@ -33,31 +33,35 @@ export const Email = () => {
       .catch((error) => {
         console.error('Error sending email:', error);
       });
+
+
   }
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col border border-gray-200 rounded-lg shadow'>
-      <input
-        type="text"
-        placeholder="Your Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="email"
-        placeholder="Your Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <textarea
-        cols="30"
-        rows="10"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      >
-      </textarea>
-      <button type="submit">Send Email</button>
+
+    <>
+    
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto w-96 mt-20">
+      
+      <div className="relative z-0 w-full mb-5 group">
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Imię, nazwisko</label>
+      </div>
+
+      <div className="relative z-0 w-full mb-5 group">
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+        <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>    
+      </div>
+     
+      <label htmlFor="message" className="block mb-2 py-2.5 px-0 w-full text-sm font-medium text-gray-900 dark:text-white"></label>
+      <textarea id="message" rows="4" value={message} onChange={(e) => setMessage(e.target.value)} className="block p-2.5 w-full h-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Treść wiadomości..."></textarea>
+
+      <button type="submit" className="mt-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Wyślij</button>
+      
     </form>
+      
+    </>
+
   )
 };
   
