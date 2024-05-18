999df8e6311bcf4accd8912f6416e55bfed1d324
Łukasz Chwalisz <lukasz.chwalisz@gmail.com>
2024-05-18 14:35:21 +0200

    A buttons on a bottom of the cards

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -15,77 +15,103 @@ const Home = () => {
                 </h1>
 
                 <div className="grid gap-4 justify-center m-4 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3">
-                    <div className="max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div className="flex flex-col items-start max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div>
                         <Link to="about">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-slate-500">O nas</h5>
                         </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-slate-300">Poczytaj o historii powstania stowarzyszenia "OSA", najciekawszych wydarzeniach, sprawozdaniach i strukturze organizacyjnej.</p>
-                        <Link to="about" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 rounded-lg  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">
+                        </div>
+                       <div className="mt-auto">
+                       <Link to="about" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 rounded-lg  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">
                             Sprawdź
                             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
                         </Link>
+                        </div>
                     </div>
-                    <div className="max-w-sm p-6 border bg-blue-50 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div className="flex flex-col items-start max-w-sm p-6 border bg-blue-50 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div>
                         <Link to="projekty">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-slate-500">Projekty</h5>
                         </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-slate-300">Aktualności z działalności stowarzyszenia "OSA": realizowane projekty, ważne komunikaty, współpraca i możliwość dofinansowania działalności stowarzyszenia.</p>
+                        </div>
+                       <div className="mt-auto">
                         <Link to="projekty" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">
                             Sprawdź
                             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
                         </Link>
+                        </div>
                     </div>
-                    <div className="max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    
+                    <div className="flex flex-col items-start max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div>
                         <Link to="wiedza"> 
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-slate-500">Wiedza</h5>
                         </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-slate-300">Zapoznaj się z najważniejszą terminologią dotyczącą zjawiska mobbingu.</p>
+                        </div>
+                        <div className="mt-auto">
                         <Link to="wiedza" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">
                             Sprawdź
                             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
                         </Link>
+                        </div>
                     </div>
-                    <div className="max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+                    <div className="flex flex-col items-start max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div>
                         <Link to="galeria">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-slate-500">Galeria</h5>
                         </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-slate-300">Galeria prac rysunkowych współpracownika stowarzyszenia - Grzegorza Krzemińskiego.</p>
+                        </div>
+                        <div className="mt-auto">
                         <Link to="galeria" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">
                             Sprawdź
                             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
                         </Link>
+                        </div>
                     </div>
-                    <div className="max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div className="flex flex-col items-start max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div>
                         <Link to="kontakt">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-slate-500">Kontakt</h5>
                         </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-slate-300">Kontakt z przedstawicielami stowarzyszenia - w centrali w Gdańsku oraz w punktach konsultacyjnych na terenie całego kraju.</p>
+                        </div>
+                        <div className="mt-auto">
                         <Link to="kontakt" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">
                             Sprawdź
                             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
                         </Link>
+                        </div>
                     </div>
-                    <div className="max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div className="flex flex-col items-start max-w-sm p-6 bg-blue-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div>
                         <Link to="form">
                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-slate-500">Formularz</h5>
                         </Link>
                         <p className="mb-3 font-normal text-gray-700 dark:text-slate-300">Istnieje możliwość kontaktu ze stowarzyszeniem bezpośrednio ze strony internetowej.</p>
+                        </div>
+                       <div className="mt-auto">
                         <Link to="form" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-blue-800">
                             Sprawdź
                             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
                         </Link>
+                        </div>
                     </div>
                 </div>
 
